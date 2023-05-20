06dbf791bee5b9e21f2195acd923fd32285489f2
gurushchuk <hanzlibnik@gmail.com>
2023-05-20 19:53:03 +0200

    created calc app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 export function App() {
-	// Declarative style below
-	const currentDate = new Date();
-	const currentYear = currentDate.getFullYear();
-	return React.createElement(
-		'div',
-		{ className: 'App' },
-		React.createElement(
-			'header',
-			{ className: 'App-header' },
-			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			React.createElement(
-				'p',
-				null,
-				'Edit ',
-				React.createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			React.createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			React.createElement('p', null, currentYear),
-		),
+	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '=', 'C'];
+	const [displayValue, setDisplayValue] = useState(0);
+	const [isResultColor, setIsResultColor] = useState(false);
+
+	const makeCalcAction = (target) => {
+		if (target === '=') {
+			try {
+				setIsResultColor(true);
+				setDisplayValue(eval(displayValue));
+			} catch (error) {
+				setDisplayValue('Ooops incorrect values');
+				setTimeout(() => setDisplayValue(0), 1000);
+			}
+		} else if (target === 'C') {
+			setIsResultColor(false);
+			setDisplayValue(0);
+		} else {
+			setIsResultColor(false);
+			const nonZeroState = displayValue === 0 ? '' : displayValue;
+			setDisplayValue(nonZeroState + target);
+		}
+	};
+
+	return (
+		<div className={styles.app}>
+			<header className={styles.header}>
+				<input
+					className={isResultColor ? styles.displayResult : styles.display}
+					value={displayValue}
+				></input>
+				<div className={styles.keypad}>
+					{numbers.map((button) => (
+						<button
+							className={styles.calcButton}
+							onClick={(event) => makeCalcAction(event.target.dataset.type)}
+							data-type={button}
+						>
+							{button}
+						</button>
+					))}
+				</div>
+				<div></div>
+			</header>
+		</div>
 	);
 }
